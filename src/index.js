16f5259cd7a7d65b8796e19298d7bf6a33163ff2
thiago-porto25 <thiago_porto25@hotmail.com>
2021-06-10 20:42:21 -0300

    create the basic logic for fetching the data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('testing')
+/* eslint-disable no-console */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable func-names */
+const asyncHandler = (function () {
+  const _key = 'f597bfe05121afdfb2f2b115097ef459'
+
+  async function getWeatherData(location) {
+    try {
+      const cleanLocation = location.toUpperCase()
+      const result = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cleanLocation}&appid=${_key}`,
+        { mode: 'cors' }
+      )
+      const bruteData = await result.json()
+      console.log(bruteData)
+      return bruteData
+    } catch (err) {
+      console.log(err)
+      return 'There was an error'
+    }
+  }
+
+  return { getWeatherData }
+})()
+
+asyncHandler.getWeatherData('London,uk')
