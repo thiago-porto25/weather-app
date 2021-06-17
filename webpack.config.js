c82bdef2ddeb311471104f90fbcd600262bbb488
thiago-porto25 <thiago_porto25@hotmail.com>
2021-06-17 17:57:21 -0300

    testing solution

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
           },
         },
       },
